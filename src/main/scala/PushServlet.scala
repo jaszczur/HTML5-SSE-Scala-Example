fdcd03a674d9074ddbd929166faaacaca104c802
Piotr Jaszczyk <piotr.jaszczyk@gmail.com>
2011-10-17 20:37:16 +0200

    working example

--- src/main/scala/PushServlet.scala
+++ src/main/scala/PushServlet.scala
@@ -1,8 +1,18 @@
 import javax.servlet.http._
 
 class PushServlet extends HttpServlet {
+  private val random = new util.Random()
+
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
-    resp.getWriter().print("Hello World!")
+    resp.setContentType("text/event-stream")
+    resp.setCharacterEncoding("utf-8")
+
+    val out = resp.getWriter()
+    out.print("data:")
+    out.println(generateData)
+    out.flush()
   }
+
+ private def generateData() = String.valueOf(random.nextInt(100) + 1)
 }
 
